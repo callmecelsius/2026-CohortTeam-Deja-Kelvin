2d414050c1cad9594437d04118d50d0399b6fcdb
Nate Levi <nathan.levi8@gmail.com>
2026-02-27 19:51:16 -0500

    fixed merge conflict

--- Frontend/types/fosterParentType.ts
+++ Frontend/types/fosterParentType.ts
@@ -1,7 +1,7 @@
 export interface FosterParent {
-    id: number;
-    userId: number;
-    fosterHomeId: number;
-    approvedDate: string;
-    status: string;
+  id: number;
+  userId?: number;
+  fosterHomeId?: number;
+  approvedDate?: string;
+  status?: string;
 }
