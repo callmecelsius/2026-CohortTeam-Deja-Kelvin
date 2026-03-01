28cd13cdfd6fb77af56f75da826e1a6241d9be26
callmecelsius <celsius.mp@gmail.com>
2026-03-01 10:44:10 -0600

    merge changes New Branch for Foster Home Table

--- Frontend/src/components/employee/FosterHomesTable.tsx
+++ Frontend/src/components/employee/FosterHomesTable.tsx
@@ -1,56 +1,25 @@
-import { getFosterHomes } from "@/api/fosterhome";
 import { getFosterParents } from "@/api/fosterparent";
-import { getUsers } from "@/api/user";
-import { DataTable } from "@/components/shared/DataTable";
-import { useEffect, useState } from "react";
-import type { FosterHome } from "types/FosterHomeType";
+// import { DataTable } from "@/components/shared/DataTable";
+// import { useEffect, useState } from "react";
 
 export function FosterHomesTable() {
-  const [fosterHomes, setFosterHomes] = useState<FosterHome[]>([]);
-  const [columns, setColumns] = useState<Array<{ header: string; accessor: string }>>([]);
-
-  useEffect(() => {
-    async function load() {
-      const homes = await getFosterHomes();
-      const parents = await getFosterParents();
-      const users = await getUsers();
+  
+//   const [columns, setColumns] = useState<Array<{ header: string; accessor: string }>>([]);
+      const parents =  getFosterParents();
       console.log(parents)
-
-      const parentsWithNames = parents.map((parent) => {
-        const user = users.find((u) => u.id === parent.userId);
-        return {
-          ...parent,
-          userName: user ? `${user.firstName} ${user.lastName}` : null,
-        };
-      });
-      
-      const homesWithParents = homes.map((home) => {
-        const homeParents = parentsWithNames.filter((p) => p.fosterHomeId === home.id);
-
-        const parentNames = homeParents.map((p) => p.userName).join(" ");
-        return {
-          ...home,
-          fosterParents: parentNames,
-        };
-      });
-      
-      setFosterHomes(homesWithParents);
-
-
-      const dynamicColumns = Object.keys(homesWithParents[0])
-        .filter((e) => !e.includes("fosterAssignments"))
-        .map((key) => ({
-          header: key,
-          accessor: key,
-        }));
-      setColumns(dynamicColumns);
-    }
-    load();
-  }, []);
+//   useEffect(() => {
+//     async function load() {
+     
+//       const parents = await getFosterParents();
+//       console.log(parents)
+//       // setColumns(parents);
+//     }
+//     load();
+//   }, []);
 
   return (
     <div className="relative w-full p-6 space-y-6">
-      <DataTable columns={columns} data={fosterHomes} />
+      {/* <DataTable columns={columns} data={fosterHomes} /> */}
     </div>
   );
 }
